68675a9b11a7053743795f3a0a24d7a0f3fc5cff
Agush <agushadad@gmail.com>
2022-10-20 20:30:26 -0300

    Configuracion de la cadena de conexion

--- IntroASP/Program.cs
+++ IntroASP/Program.cs
@@ -1,7 +1,14 @@
+using IntroASP.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ASPNETContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ASPNETContext"));
+});
 
 var app = builder.Build();
 
