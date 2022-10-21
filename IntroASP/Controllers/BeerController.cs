b49d78ee8acf836c629302d6c95b223ca8a7c6ed
Agush <agushadad@gmail.com>
2022-10-20 21:03:38 -0300

    Creacion de controladores

---
+++ IntroASP/Controllers/BeerController.cs
@@ -0,0 +1,22 @@
+﻿using IntroASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IntroASP.Controllers
+{
+    public class BeerController : Controller
+    {
+        private readonly ASPNETContext _context;
+
+        public BeerController (ASPNETContext context) 
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var beers = _context.Beers.Include(x => x.Brand);
+            return View(await beers.ToListAsync());
+        }
+    }
+}
---
+++ IntroASP/Controllers/BrandController.cs
@@ -0,0 +1,16 @@
+﻿using IntroASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly ASPNETContext _context;
+
+        public BrandController(ASPNETContext context) => _context = context;
+
+
+        public async Task<IActionResult> Index() => View(await _context.Brands.ToListAsync());
+    }
+}
